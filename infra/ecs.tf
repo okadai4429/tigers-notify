9ca27078806450cf5a9a078f400ccdb432e58e4a
Daiki_Okamoto <okaokadai1008@gmail.com>
2026-08-30 13:27:22 +0900

    terraform: iam.tf と ecs.tf を追加

---
+++ infra/ecs.tf
@@ -0,0 +1,30 @@
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-cluster"
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = "${var.project_name}"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([{
+    name  = "${var.project_name}"
+    image = "${var.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.project_name}:latest"
+    environment = [{
+      name  = "SLACK_WEBHOOK_URL"
+      value = var.slack_webhook_url
+    }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        "awslogs-group"         = "/ecs/${var.project_name}"
+        "awslogs-region"        = var.aws_region
+        "awslogs-stream-prefix" = "ecs"
+        "awslogs-create-group"  = "true"
+      }
+    }
+  }])
+}
